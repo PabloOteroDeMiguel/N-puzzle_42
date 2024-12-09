f9783b66c2384c8b8583e0dff2929e4df0850cb3
Pablo Otero De Miguel <potero-d@c4r4s1.42urduliz.com>
2024-12-09 10:45:08 +0100

    shuffle a vector

--- src/Game.cpp
+++ src/Game.cpp
@@ -6,12 +6,14 @@
 /*   By: potero-d <potero-d@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/05 12:55:50 by potero-d          #+#    #+#             */
-/*   Updated: 2024/12/05 14:07:59 by potero-d         ###   ########.fr       */
+/*   Updated: 2024/12/09 10:44:34 by potero-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/Game.h" 
 #include "../include/Npuzzle.h"
+#include <bits/stdc++.h>
+#include <random>
 
 // Constructor
 
@@ -36,17 +38,24 @@ Game::~Game() {
 // Methods
 void Game::generateGame() {
     
-    // int cont = 0;
+    int cont = 1;
     int max = this->size * this->size;
     std::vector<int> aux;
     for (int i = 0; i < max; i++){
-        aux.push_back(i);
+        this->numbers.push_back(i);
     }
-    for (auto iter : aux)
-    {
-        numbers.push_back(iter);
-    }
+    
+    // Shuffle a vector (random)
+    // Initialize random number generator --> https://www.geeksforgeeks.org/how-to-shuffle-a-vector-in-cpp/
+    std::random_device rd;
+    std::mt19937 g(rd());
+    shuffle(numbers.begin(), numbers.end(), g);
+    
     for(int number : this->numbers) {
-        std::cout << number << ", " << std::endl;
+        std::cout << number << "  ";
+        if (cont % this->size == 0){
+            std::cout << std::endl;
+        }
+        cont++;
     }
 }
